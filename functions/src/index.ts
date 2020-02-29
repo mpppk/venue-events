a494604f9c3378b1c26d1f5f16db0ca403d7a029
mpppk <niboshiporipori@gmail.com>
2020-02-29 09:18:12 +0000

    Add prettier

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,9 @@
-import * as functions from 'firebase-functions'
-import { JSDOM } from 'jsdom';
+import * as functions from "firebase-functions";
+import { JSDOM } from "jsdom";
 
 export const helloWorld = functions.https.onRequest((request, response) => {
-    JSDOM.fromURL("https://example.com/", {}).then(dom => {
-        console.log(dom.serialize());
-        response.send('Hello from Firebase!!!!!\n\n');
-    });
+  JSDOM.fromURL("https://example.com/", {}).then(dom => {
+    console.log(dom.serialize());
+    response.send("Hello from Firebase!\n\n");
+  });
 });
